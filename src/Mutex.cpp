3b0a3cdffc815281326f1140dde5c1de0baf12e5
Geoffrey Hunter <gbmhunter@gmail.com>
2025-06-13 21:24:34 +1200

    Add lockGuard() function to Mutex.

--- src/Mutex.cpp
+++ src/Mutex.cpp
@@ -2,14 +2,41 @@
 
 namespace zephyr_cpp_toolkit {
 
+//================================================================================================//
+// MutexLockGuard
+//================================================================================================//
+
+MutexLockGuard::MutexLockGuard(Mutex& mutex, k_timeout_t timeout)
+    : m_mutex(mutex)
+{
+    // The mutex is locked when the lock guard is constructed.
+    k_mutex_lock(m_mutex.getZephyrMutex(), timeout);
+}
+
+MutexLockGuard::~MutexLockGuard()
+{
+    // Guard is being destroyed, so the mutex is unlocked.
+    k_mutex_unlock(m_mutex.getZephyrMutex());
+}
+
+//================================================================================================//
+// Mutex
+//================================================================================================//
+
 Mutex::Mutex()
 {
     // Create Zephyr mutex
-    k_mutex_init(&m_mutex);
+    k_mutex_init(&m_zephyrMutex);
 }
 
 Mutex::~Mutex()
 {
+}
+
+MutexLockGuard Mutex::lockGuard(k_timeout_t timeout)
+{
+    // Create a lock guard, passing in this mutex
+    return MutexLockGuard(*this, timeout);
 }
 
 } // namespace zephyr_cpp_toolkit
--- src/Mutex.hpp
+++ src/Mutex.hpp
@@ -4,12 +4,46 @@
 
 namespace zephyr_cpp_toolkit {
 
+// Forward declarations
+class Mutex;
+
+/** 
+ * A RAII class that locks the mutex when constructed and unlocks it when destroyed.
+ */
+class MutexLockGuard {
+public:
+    MutexLockGuard(Mutex& mutex, k_timeout_t timeout = K_FOREVER);
+    ~MutexLockGuard();
+private:
+    // The mutex this guard is locking/unlocking.
+    Mutex& m_mutex;
+};
+
 class Mutex {
 public:
     Mutex();
     ~Mutex();
+
+    /**
+     * @brief Create a lock guard for the mutex which locks the mutex when constructed and unlocks it when destroyed.
+     * 
+     * This is a RAII class that ensures the mutex is unlocked when it goes out of scope. You don't normally need to unlock the mutex manually.
+     * 
+     * @param timeout The max. time to wait to lock the mutex.
+     * @return MutexLockGuard  The lock guard for the mutex.
+     */
+    MutexLockGuard lockGuard(k_timeout_t timeout = K_FOREVER);
+
+    /**
+     * @brief Get the underlying Zephyr mutex object.
+     * 
+     * It is not recommended to use this function. Only use as an escape hatch.
+     * 
+     * @return A pointer to the Zephyr mutex object.
+     */
+    struct k_mutex* getZephyrMutex() const;
 private:
-    struct k_mutex m_mutex;
+    struct k_mutex m_zephyrMutex;
 };
 
 } // namespace zephyr_cpp_toolkit
